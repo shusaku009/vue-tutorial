2199429bfa15b1ebea89484c8fc4726e50275aa8
shusaku009 <shusaku009@gmail.com>
2025-10-13 11:02:47 +0900

    add: 条件付きレンダリング

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ const message = ref("Hello World!");
 const titleClass = ref("title");
 const text = ref("");
 const modeltext = ref("");
+const awesome = ref(true);
 
 function incrementCount() {
   count.value++;
@@ -16,6 +17,10 @@ function onInput(e: Event) {
   const target = e.target as HTMLInputElement;
   if (!target) return;
   text.value = target.value;
+}
+
+function toggleAwesome(): void {
+  awesome.value = !awesome.value;
 }
 </script>
 
@@ -29,6 +34,10 @@ function onInput(e: Event) {
   <p>{{ text }}</p>
   <div><input type="text" v-model="modeltext" placeholder="Type here" /></div>
   <p>{{ modeltext }}</p>
+  <button @click="toggleAwesome">Toggle</button>
+  <h1>{{ awesome ? "Awesome" : "Not Awesome" }}</h1>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Vue is not awesome!</h1>
 </template>
 
 <style>

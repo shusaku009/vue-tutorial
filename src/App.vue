fbf2b315fa0879a8c82f8822d3c8fecbcbdcfb14
shusaku009 <shusaku009@gmail.com>
2025-10-13 09:20:34 +0900

    add: イベントリスナー

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,21 @@
 import { reactive, ref } from "vue";
 
 const counter = reactive({ count: 0 });
+const count = ref(0);
 const message = ref("Hello World!");
 const titleClass = ref("title");
+
+function incrementCount() {
+  count.value++;
+}
 </script>
 
 <template>
   <h1>{{ message }}</h1>
   <p>Count is: {{ counter.count }}</p>
   <h1 :class="titleClass">Make me red</h1>
+  <button @click="incrementCount()">countUp</button>
+  {{ count }}
 </template>
 
 <style>

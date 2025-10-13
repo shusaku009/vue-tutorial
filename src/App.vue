c4df6f293c9435465b2e877582cc313f5489d7aa
shusaku009 <shusaku009@gmail.com>
2025-10-13 10:48:57 +0900

    add: フォームバインディング

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,16 @@ const counter = reactive({ count: 0 });
 const count = ref(0);
 const message = ref("Hello World!");
 const titleClass = ref("title");
+const text = ref("");
 
 function incrementCount() {
   count.value++;
+}
+
+function onInput(e: Event) {
+  const target = e.target as HTMLInputElement;
+  if (!target) return;
+  text.value = target.value;
 }
 </script>
 
@@ -17,6 +24,8 @@ function incrementCount() {
   <h1 :class="titleClass">Make me red</h1>
   <button @click="incrementCount()">countUp</button>
   {{ count }}
+  <div><input :value="text" @input="onInput" placeholder="Type here" /></div>
+  {{ text }}
 </template>
 
 <style>
